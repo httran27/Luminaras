cc8dfbe24631117597d4a45874169de9ecef1eb9
vij8085 <39298986-vij8085@users.noreply.replit.com>
2025-02-02 15:26:54 +0000

    Agent query: Can you try searching for 'marmun292' in the search bar and press Enter? Does it navigate to their profile?
    Add basic navbar component.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/0790f569-f762-417f-8fe1-d24ed39518eb/7d290baf-5264-4c04-8eb6-8e79885a53ff.jpg

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -75,6 +75,7 @@ export default function Navbar() {
     }
   };
 
+
   return (
     <nav className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center px-4">
